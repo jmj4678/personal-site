80c615fde6be30d6cba2bd20ff1511dec226c340
Jose Maria Jatem <chemajatem@gmail.com>
2024-03-05 22:58:31 -0600

    Saving progress

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -13,156 +13,98 @@
  */
 const work = [
   {
-    name: 'Smile Identity',
-    position: 'VP Engineering & Head of AI',
-    url: 'https://smileidentity.com',
-    startDate: '2022-01-01',
-    summary: `Smile builds machine learning APIs to compare user submitted photos to ID documents. Our APIs are
-    used by hundreds of thousands of people every day to access financial services. As VP of Engineering,
-    I provide technical leadership on high-impact projects, influence and coach a distributed team of engineers,
-    and facilitate alignment and clarity across teams on goals, outcomes, and timelines. I was promoted
-    from Director of Engineering to VP of Engineering in April 2022, and then to VP of Engineering and Head
-    of AI in November 2022. I lead a 20+ person engineering org. I directly manage ~8 engineers and spend
-    >50% of my time writing code.`,
+    name: 'Control Risks',
+    position: 'Data Analytics Consultant',
+    url: 'https://controlrisks.com',
+    startDate: '2022-02-01',
+    summary:
+    'Control Risks is a firm specialized in risk consultancy that helps to create secure, compliant and resilient organizations. As a consultant in the data science industry, we have developed a team of engineers that are dedicated to the business and provide the best services to our customers. I provide technical leadership on high-impact projects, influence and coach a distributed team of engineers, and facilitate alignment and clarity across teams on goals, outcomes, and timelines.\n',
     highlights: [
-      'Redesigned engineering processes for bug tracking, meetings, and standups. Improved culture for code reviews, blameless post-mortems, and retrospectives.',
-      'Re-architected engineering hiring and onboarding processes. Recruited several strong engineers.',
-      'Reorged engineering teams to focus on product delivery. Created a new team to focus on ML infrastructure.',
-      'Lead re-design of internal APIs for inference. Built new computer vision pipelines for industry leading certifications (NIST/iBeta liveness).',
-      'Pitched, designed, developed, deployed, and maintain a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
+      'Redesigned engineering processes for continuous client engagements.',
+      'Re-architected and migrated data processing to ensure we use the latest cloud tools.',
+      'Established and nurtured strong client relationships through regular communication and project updates.',
+      'Built new computer vision and ETL pipelines',
+      'Implemented and maintained dashboards for key internal and external decision makers',
     ],
   },
   {
-    name: 'Skeptical Investments',
+    name: 'Seax Knives',
     position: 'Co-founder',
-    url: 'http://skepticalinvestments.biz',
-    startDate: '2017-04-01',
-    summary: `Skeptical Investments is a micro-vc fund focused on Angel, Seed, and Series-A
-    YC companies.`,
-    highlights: [
-      'Created the InstaSafe. Fast, automated investment docs for YC companies.',
-      'Advise several startups in the fund.',
-    ],
-  },
-  {
-    name: 'Arthena',
-    position: 'Co-founder & C.T.O.',
-    url: 'https://arthena.com',
-    startDate: '2014-01-01',
-    endDate: '2022-01-01',
-    summary: `Arthena is a Series A Company funded by <a href="https://www.anthemis.com/">Athemis</a>,
-    <a href="https://foundationcapital.com">Foundation Capital</a>, and <a href="https://ycombinator.com/">YC</a>.
-    We develop quantitative strategies to predict the value of fine art and build investment products.
-    I ran a 20-person product and engineering org for 8 years. We were acquired by
-    <a href="https://www.masterworks.com/">Masterworks</a> in 2023.`,
+    url: 'https://seaxknives.com',
+    startDate: '2023-09-01',
+    summary: `Seax Knives is a sidekick me and two friends started last year.
+    We import handmade knives from Mexico, and sell them in the US through our
+    website, and through partnerships with local stores. We have automated
+    most of the work and at this point our concern is to match supply with demand,
+    which is naturally a very hard thing to accomplish! It's been a sweet source of passive income.`,
     highlights: [
       'Built production, online, end-to-end optimized machine learning pipelines for time-series prediction.',
-      'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-      "Set and communicated team priorities that supported the broader organization's goals. Aligned strategy, processes, and decision-making across teams.",
-      "Set clear expectations with individuals based on their level and role and aligned them to the broader organization's goals. Met regularly with individuals to discuss performance and development, and provided feedback and coaching.",
-      'Developed the long-term technical vision and roadmap within, and often beyond, the scope of my teams. Evolved the roadmap to meet anticipated future requirements and infrastructure needs.',
+      "Set and communicated team priorities that supported the broader organization's goals. Aligned strategy, processes, and decision-making across team.",
+      'Collaborated with regional partners to uphold product excellence, ensuring adherence to quality standards and optimizing the sales funnel for sustained business growth.',
     ],
   },
   {
-    name: 'Enveritas',
-    position: 'ML Engineering Contractor',
-    url: 'http://enveritas.org',
-    startDate: '2016-09-01',
-    endDate: '2018-01-01',
-    summary: `Enveritas is a non-profit that works with coffee farmers to improve their livelihoods.
-    I joined on day one and helped build a variety of tools to collect and analyze data. I also advised
-    on technical strategy, helped hire engineers, and advised on their YC application.`,
+    name: 'Pecus',
+    position: 'Co-founder & COO',
+    url: 'http://pecus.app',
+    startDate: '2021-12-01',
+    endDate: '2023-12-31',
+    summary: `Pecus is an organization that is dedicated to the virtualization and data transformation of the key members in the agricultural industry within the United States,
+    with special focus on Livestock Breeders.`,
     highlights: [
-      'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
-      'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
-      'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM\'s, and other techniques.',
+      'Revitalized the business model by aligning our core purpose with our messaging, and spearheaded marketing campaigns to attract breeders to our network.',
+      'Successfully graduated from the prestigious Builders and Backers Accelerator program.',
     ],
   },
   {
-    name: 'Zenysis',
-    position: 'Software Engineering Contractor',
-    url: 'https://zenysis.com',
-    startDate: '2016-02-01',
-    endDate: '2016-03-01',
-    summary: `Zenysis develops data analysis and visualization tools for government agencies and international organizations.
-    Their solutions help improve decision-making by integrating and analyzing large-scale datasets from various sources.
-    I was the second contributor to the codebase. We built data visualization tools and tackled complex data ingestion
-    challenges for the Ethiopian Ministry of Health.`,
+    name: 'Atento Capital',
+    position: 'Data Analytics Intern',
+    url: 'https://www.atentocapital.com/',
+    startDate: '2021-06-01',
+    endDate: '2021-08-15',
+    summary: `Atento is an early-stage investment firm focused on unlocking unsung potential.
+    We identify high-potential ideas and individuals and then invest the early-stage funding and human capital needed to take ventures from 0-1.
+    By bringing together founders, partners, and investors, we are transforming the heartland into the nation's most equitable tech hub while generating real returns.`,
     highlights: [
-      'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
-      'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
+      'Designed and implemented Power BI dashboards for the Marketing team, seemlessly integrating diverse data sources such as MailChimp, Google Analytics, CRM systems, and various other sources.',
+      'Produced cloud-based solutions and automation tools to streamline data processing workflows, reducing operational costs, and ensuring data quality.',
+      'Trained a descriptive machine learning to provide strategic risk assessments to decision makers',
+      'Implemented cloud storage solutions through BigQuery and PostgreSQL, improving data accessibility and scalability.',
     ],
   },
   {
-    name: 'Matroid',
-    position: 'Co-founder',
-    url: 'https://matroid.com',
+    name: 'University of Tulsa',
+    position: 'Graduate Assistant',
+    url: 'https://utulsa.edu/',
+    startDate: '2019-11-01',
+    endDate: '2021-12-17',
+    summary: "Engaged in impactful roles at the University of Tulsa, a distinguished academic institution renowned for its commitment to excellence in education and research. Collaborated within the business college to support research and student success, contributing to the university's mission of fostering knowledge, innovation, and academic excellence.\n",
+    highlights: [
+      'Worked with professors and students with research and development.',
+    ],
+  },
+  {
+    name: 'Empresas Polar',
+    position: 'Quality Analyst',
+    url: 'https://empresaspolar.com',
     startDate: '2015-07-01',
     endDate: '2016-01-01',
-    summary: `Matroid is a computer vision platform for creating and deploying detectors. Detectors help customers identify objects,
-    events, or patterns in images and videos without requiring machine learning or coding expertise. I played a crucial role in
-    defining the company's vision. I was responsible for architecting and building the initial product. I left shortly after the
-    Series A to focus on Arthena.`,
+    summary: "Contributed to the success of Empresas Polar, a prominent Venezuelan conglomerate renowned for its leadership in the food and beverage industry. Worked within a dynamic and challenging environment, actively participating in CPG manufacturing to support the company's continued growth and impact in the Venezuelan business landscape.\n",
     highlights: [
-      'Developed end to end machine learning pipeline to train visual classifiers from keywords using Caffe, Node.JS, Redis, MongoDB, and other technologies.',
+      'Implemented Lean Six Sigma methodologies, reducing packaging defects by 20%.',
+      'Led adoption of data-driven practices, boosting operational efficiencies, and providing training to solidify team members tech stack.',
+      'Spearheaded internal manufacturing initiatives that decreased monthly customer dissatisfaction claims by 33%.',
     ],
   },
   {
-    name: 'Planet',
-    position: 'Missions Intern',
-    url: 'https://planet.com',
+    name: 'Chevron',
+    position: 'Facilities Engineer Intern',
+    url: 'https://www.chevron.com/',
     startDate: '2014-06-01',
     endDate: '2015-01-01',
     highlights: [
-      'Built models to improve image quality, signal to noise ratio, and dynamic range.',
-      'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
-      'Organized first hackathon, prototyped hardware, and designed and built photography equipment for rocket launches.',
-    ],
-  },
-  {
-    name: 'Planetary Resources',
-    position: 'Avionics Intern',
-    startDate: '2014-01-01',
-    endDate: '2014-05-01',
-    url: 'http://planetaryresources.com',
-    highlights: [
-      'Developed simulations in Matlab for Attitude Determination and Control Subsystem.',
-      'Developed processes for in lab testing and characterization of various subsystems.',
-      'Assembled flight hardware in cleanroom.',
-    ],
-  },
-  {
-    name: 'Facebook',
-    position: 'Software Engineer Intern',
-    url: 'https://facebook.com',
-    startDate: '2013-06-01',
-    endDate: '2013-09-01',
-    highlights: [
-      'Developed software in python for automated testing of servers.',
-      'Performed statistical analysis with R, HIVE to assist in triage of malfunctioning servers.',
-      'Worked with vendors and ODM\'s during triage to assist in risk mitigation.',
-    ],
-  },
-  {
-    name: 'SEDS-USA',
-    position: 'At Large Board Member',
-    url: 'http://seds.org',
-    startDate: '2013-10-01',
-    endDate: '2014-10-01',
-    highlights: [
-      'Elected to Board of Directors of the USA\'s largest student space advocacy group based on 5+ years of work with SEDS.',
-      'Responsibilities included: organizational strategy, conference presentations, fundraising, special projects, promoting SEDS nationally and internationally, and photographing conferences.',
-    ],
-  },
-  {
-    name: 'UB Nanosatellite Program',
-    position: 'Co-founder, Program Manager',
-    url: 'https://ubnl.space/',
-    startDate: '2010-10-01',
-    endDate: '2012-06-01',
-    highlights: [
-      'Coauthored grant to build a multi-spectral imaging satellite as part of the AFRL University Nanosatellite Program.',
-      'Lead a team of 60 students through satellite development life cycle. Served as a technical expert, acquired intimate working knowledge of satellite subsystems.  Solicited funding through NASA, AFOSR, and several corporate sponsors.',
+      'Developed a simulation of all the onshore surface infrastructure across 240 square miles of oilfield with over 500 active wells. Leveraged python to ensure data quality.',
+      'Provided gas recovery and construction recommendations, representing a 35% cost of installation reduction.',
     ],
   },
 ];
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
 
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact Jose Jatem via email @ chemajatem@gmail.com"
   >
     <article className="post" id="contact">
       <header>
@@ -18,7 +17,7 @@ const Contact = () => (
       </header>
       <div className="email-at">
         <p>Feel free to get in touch. You can email me at: </p>
-        <EmailLink />
+        <a href="mailto:chemajatem@gmail.com">chemajatem@gmail.com</a>
       </div>
       <ContactIcons />
     </article>
